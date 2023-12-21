6566f88d3166f752386c1efe7e82085365b2e526
Alikhan Sailybaev <sailybaevvv@gmail.com>
2023-12-21 20:14:33 +0600

    Final

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 public class MyApplication {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        Shape shape = new Shape();
-        int n = scan.nextInt();
-        for(int i=0;i<n;i++) {
-            double x = scan.nextInt();
-            double y = scan.nextInt();
-            Point point = new Point(x,y);
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File inpt = new File("/Users/sailybaev/IdeaProjects/MrKhaimuldin/src/input.txt");
+        Scanner read = new Scanner(inpt);
+        Shape shape = new Shape();
+
+        while (read.hasNext()) {
+
+            String s = read.next();
+            double x = Double.valueOf(s.split(",")[0]);
+            double y = Double.valueOf(s.split(",")[1]);
+
+            Point point = new Point(x, y);
             shape.addPoint(point);
         }
+        
 
         double per = shape.calculatePerimeter();
         double avgsd = shape.getAverageSide();
         double lngstsd = shape.getLongestSide();
 
-        System.out.println(per + "\n" + avgsd + "\n" + lngstsd + "\n");
+        System.out.println(per + " - Perimeter\n" + avgsd + " - Average Side\n" + lngstsd + " - Longest Side\n");
 
 
     }
